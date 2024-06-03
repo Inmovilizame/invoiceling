524813bb2cd43c35397efa7513d104eae9119758
Roi Rodríguez <info@inmoviliza.me>
2024-06-03 14:23:29 +0200

    feat: update version and release action

--- cmd/cli/commands/root.go
+++ cmd/cli/commands/root.go
@@ -12,7 +12,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "invoiceling",
-	Version: "v0.0.4",
+	Version: "v0.0.5",
 	Short:   "CLI based invoicing tool for freelancers",
 	Long: `Simple CLI tool to manage invoice for freelancers.
 	Generate, store and transform to pdfObject.`,
